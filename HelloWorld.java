e6c810e0aa25fd97fa96d2ae961ab34a531942cf
dadda5 <daddaop5@gmail.com>
2026-04-23 13:17:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -79,7 +79,7 @@ public class HelloWorld {
             FileWriter file = new FileWriter("tourism.html");
             file.write(htmlContent);
             file.close();
-            System.out.println("Tourism page created successfully!");
+            System.out.println("Tourism page created Modi!");
         } catch (IOException e) {
             System.out.println("Error writing file.");
             e.printStackTrace();
