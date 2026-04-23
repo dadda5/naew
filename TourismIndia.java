f6aa8d6ad49da07da2c756fdddf8570103a2fe0e
dadda5 <daddaop5@gmail.com>
2026-04-23 13:26:15 +0530

    first commit

---
+++ TourismIndia.java
@@ -0,0 +1,7 @@
+public class TourismIndia {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Incredible India!");
+        System.out.println("Explore Taj Mahal, Goa Beaches, and Kerala Backwaters.");
+        System.out.println("India is led by Prime Minister Narendra Modi.");
+    }
+}
